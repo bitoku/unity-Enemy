ce4f65d58c5432e264b4a6a869dfd4ee84228739
bamura <bamura95@gmail.com>
2019-07-14 17:13:30 +0900

    時間表示機能を入れた

---
+++ Assets/Scripts/Manager/TimeManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    public static float time;
+    public Image UIobj;
+    public Text timeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0f;
+        UIobj.fillAmount = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        int RemainTime = 30 - Mathf.RoundToInt(time);
+
+        if ( time <= 30f )
+        {
+            UIobj.fillAmount += Time.deltaTime / 30f ;
+        }
+
+        if( 0 <= RemainTime && time <= 30f )
+        {
+            timeText.text = RemainTime.ToString();
+        }
+    }
+}
