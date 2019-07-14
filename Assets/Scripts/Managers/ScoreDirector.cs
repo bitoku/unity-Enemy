ae71c6268fd652b2aad1cb1a48e1bf6a46a9132b
bamura <bamura95@gmail.com>
2019-07-14 21:04:07 +0900

    StartSceneにRESTARTボタンを追加

--- Assets/Scripts/Managers/ScoreDirector.cs
+++ Assets/Scripts/Managers/ScoreDirector.cs
@@ -2,18 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ScoreDirector : MonoBehaviour
 {
     GameObject score1UI;
     GameObject score2UI;
 
+    public Text Winer1;
+    public Text Winer2;
+
     void Start()
     {
         score1UI = GameObject.Find("Score1");
         score2UI = GameObject.Find("Score2");
+
         score1UI.GetComponent<Text>().text = ScoreManager.score1.ToString();
         score2UI.GetComponent<Text>().text = ScoreManager.score2.ToString();
+
+        if(ScoreManager.score1 > ScoreManager.score2)
+        {
+            Winer1.text = "WINER!!";
+            Winer2.text = "";
+        }
+        if (ScoreManager.score1 < ScoreManager.score2)
+        {
+            Winer1.text = "";
+            Winer2.text = "WINER!!";
+        }
+        if (ScoreManager.score1 == ScoreManager.score2)
+        {
+            Winer1.text = "DRAW";
+            Winer2.text = "DRAW";
+        }
     }
 
     public void ScoreUp1()
@@ -26,5 +47,10 @@ public class ScoreDirector : MonoBehaviour
     {
         ScoreManager.score2 += 100;
         score2UI.GetComponent<Text>().text = ScoreManager.score2.ToString();
+    }
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene("GameScene");
     }
 }
